d187554d5c187263d877b4b7598acf743f38559d
TylerJac <sans.gaming235@gmail.com>
2024-07-03 11:29:31 -0400

    finished work, first draft

--- app.js
+++ app.js
@@ -1,12 +1,30 @@
 import express from 'express';
+import bodyParser from 'body-parser';
+import morgan from 'morgan';
 
 const app = express();
 
+app.use(morgan('dev'));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.send('Hello to the server');
 });
 
-app.listen(3000, () => {
-  console.log('Example app listening on port 3000!');
+app.get('/about', (req, res) => {
+    res.send('This server is built to demonstrate basic Express setup with middleware.');
 });
+
+app.use((req, res, next) => {
+    res.status(404).send('Page Not Found');
+});
+
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
 
